4cf590746d08612bb34f8f9471592b7433c35022
anilmaurya61 <anilkumarmaurya246@gmail.com>
2023-10-22 00:01:29 +0530

    Drill 2

--- index.js
+++ index.js
@@ -1,63 +1,40 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 const { v4: uuidv4 } = require('uuid');
+const path = require('path');
 
-const server = http.createServer((req, res) => {
-    let urlPath = req.url.split('/');
-    let route = urlPath[1];
-    let subRoute = urlPath[2];
-
-    console.log(route, subRoute)
-    if (route === 'html') {
-        fs.readFile('home.html', 'utf8', (err, data) => {
-            if (err) {
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end('Internal Server Error');
-            } else {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end(data);
-            }
-        });
-    }
-    else if (route === 'json') {
-        fs.readFile('index.json', (err, data) => {
-            if (err) {
-                res.writeHead(500, 'utf-8', { 'Content-Type': 'text/plain' });
-                res.end('Internal Server Error');
-            } else {
-                const jsonData = JSON.parse(data);
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify(jsonData, null, 2));
-            }
-        });
-    }
-    else if (route === 'uuid') {
-        let uuid = uuidv4();
-        const jsonResponse = {
-            uuid: uuid
-        };
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(jsonResponse, null, 2));
-    }
-    else if (route === 'status' && subRoute) {
-        res.writeHead(subRoute, { 'Content-Type': 'text/plain' });
-        res.end(`Responce with status code ${subRoute}`)
-    }
-    else if (route === 'delay' && subRoute) {
-        let time = parseInt(subRoute);
-        setTimeout((subRoute) => {
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end(`Response with a delay of ${time}`)
-        }, 3000)
-    }
-    else {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end('<html><body><h1>Page not found</h1></body></html>');
-    }
-});
-
+const app = express();
 const portNumber = 8000;
 
-server.listen(portNumber, () => {
+app.use(express.static(path.resolve(__dirname, './public')));
+app.get('/html', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, './public/home.html'));
+});
+
+app.get('/json', (req, res) => {
+   res.sendFile(path.resolve(__dirname, './public/index.json'));
+});
+
+app.get('/uuid', (req, res) => {
+    const uuid = uuidv4();
+    res.status(200).json({ uuid });
+});
+
+app.get('/status/:statusCode', (req, res) => {
+    const statusCode = parseInt(req.params.statusCode);
+    res.status(statusCode).send(`Response with status code ${statusCode}`);
+});
+
+app.get('/delay/:time', (req, res) => {
+    const time = parseInt(req.params.time);
+    setTimeout(() => {
+        res.status(200).send(`Response with a delay of ${time}`);
+    }, time);
+});
+
+app.get('*', (req, res) => {
+    res.status(404).send('<html><body><h1>Page not found</h1></body></html>');
+});
+
+app.listen(portNumber, () => {
     console.log(`Server is running on http://localhost:${portNumber}`);
 });
